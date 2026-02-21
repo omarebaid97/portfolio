daabd02bbd9325d1e19b8489ff69fa5b71389926
omarebaid97 <156451357+omarebaid97@users.noreply.github.com>
2026-02-20 23:16:38 -0600

    Update Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,42 +1,40 @@
-export default function Services() {
+import Image from 'next/image'
+import Link from 'next/link'
+import { BriefcaseIcon } from '@heroicons/react/24/outline'
+import { ArrowDownOnSquareIcon } from '@heroicons/react/24/solid'
+
+export default function Work() {
     return (
-        <div className="bg-base-100 my-20 lg:px-20 w-full flex flex-col justify-between">
-            <div className="flex flex-col space-y-3 mb-10 lg:p-0 p-5">
-                <h1 className="text-5xl font-bold leading-normal text-base-content max-w-2xl">Here are some of the things I can do for you!</h1>
-                <p className="text-lg text-base-content max-w-4xl">I love taking on new challenges, so if you have an idea for something different let me know! I&apos;m always looking for new ways to learn and grow.</p>
+        <div className="stats stats-vertical lg:w-1/3 w-11/12 mx-auto my-10 p-5 rounded-2xl border border-accent shadow-2xl shadow-secondary">
+            <div className="flex mr-5 space-x-5 mb-5">
+                <BriefcaseIcon className="w-8 h-8 text-accent ml-5" />
+                <h2 className="text-3xl font-bold text-accent">Work</h2>
             </div>
-            <div className="flex lg:flex-row flex-col lg:space-x-16 space-y-20 my-5 lg:p-0 p-5">
-                <div className="card w-full lg:w-1/3 shadow-2xl shadow-secondary">
-                    <div className="card-body">
-                        <h2 className="card-title text-secondary">
-                            Development
-                        </h2>
-                        <p>
-                            I can build you anything from scratch, or help you with an existing software. I have experience with Frontend Development, Python, and plenty of other languages and technologies.
-                        </p>
-                    </div>
+            <div className="stat">
+                <div className="stat-figure bg-base-300 h-20 items-center flex p-5 rounded-lg">
+                    <Image src="/veritas-logo.png" alt="Logo for Veritas" width={100} height={100} />
                 </div>
-                <div className="card w-full lg:w-1/3 shadow-2xl shadow-secondary">
-                    <div className="card-body">
-                        <h2 className="card-title text-secondary">
-                            Consulting
-                        </h2>
-                        <p>
-                            I can provide some guidance and help steer things, whether it&apos;s a website, app, SEO, or something else. I&apos;ve been working with technology for over 10 years, and I&apos;m always learning new things.
-                        </p>
-                    </div>
-                </div>
-                <div className="card w-full lg:w-1/3 shadow-2xl shadow-secondary">
-                    <div className="card-body">
-                        <h2 className="card-title text-secondary">
-                            Non-Profits
-                        </h2>
-                        <p>
-                            I&apos;m always looking for ways to give back to the community. If you&apos;re a non-profit organization, I&apos;d love to help you out for free.
-                        </p>
-                    </div>
-                </div>
+                <div className="stat-value text-2xl">Veritas</div>
+                <div className="stat-desc">CTRM Consultant</div>
+                <div className="stat-desc">Nov 2023 - Present</div>
             </div>
+            <div className="stat">
+                <div className="stat-figure bg-base-300 h-20 items-center flex p-5 rounded-lg">
+                    <Image src="/unlock-logo.png" alt="Logo for Unlock Health" width={100} height={100} />
+                </div>
+                <div className="stat-value text-2xl">Unlock Health</div>
+                <div className="stat-desc">Software Engineer</div>
+                <div className="stat-desc">Feb 2022 - Nov 2023</div>
+            </div>
+            <div className="stat">
+                <div className="stat-figure bg-base-300 h-20 items-center flex p-5 rounded-lg">
+                    <Image src="/fuzati-logo.svg" alt="Logo for Fuzati" width={100} height={100} />
+                </div>
+                <div className="stat-value text-2xl">Fuzati</div>
+                <div className="stat-desc">Frontend Developer</div>
+                <div className="stat-desc">Feb 2019 - May 2022</div>
+            </div>
+            <Link href="https://github.com/oebaid97/portfolio/raw/main/public/resume.pdf" target="_blank" className="btn btn-accent max-w-sm flex text-base-100">Download Resume <ArrowDownOnSquareIcon className='fill-base-100 font-bold h-5 w-5 mb-2' /></Link>
         </div>
     )
 }
